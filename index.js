61c85757a78206992330697466fb6e77e7c3eab1
HaiquanZ <nn764562@gmail.com>
2024-04-16 14:33:03 +0700

    update router and auth middleware

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+const authorize = require('./src/middlewares/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', authorize, proxy('http://localhost:8001'))
+app.use('/group', authorize, proxy('http://localhost:8002'))
+app.use('/assignment', authorize, proxy('http://localhost:8003'))
+
+app.listen(8000, () => {
+    console.log('Gateway listening on 8000')
+})
---
+++ src/middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authorize = function (req, res, next) {
+    try {
+        //console.log(req.url);
+        if(req.url === "/login" || req.url === "/register"){
+            return next();
+        }
+        const authorizationHeader = req.headers.authorization;
+        if (!authorizationHeader) throw new Error("Unauthorized");
+
+        const token = req.headers.authorization.split(" ")[1];
+
+        return jwt.verify(token, JWT_SECRET, function (err, data) {
+            if (err) throw new Error("Authorize Failed");
+            req.user = data;
+            next();
+        });
+    } catch (err) {
+        return res.status(401).json({
+            message: err.message
+        });
+    }
+};
+
+module.exports = authorize;
