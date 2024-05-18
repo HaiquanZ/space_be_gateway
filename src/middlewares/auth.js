f219f4779de1db17db0b67a157ae8e5cbb28b547
HaiquanZ <nn764562@gmail.com>
2024-05-18 23:52:32 +0700

    update 18/05

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,6 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const authorize = function (req, res, next) {
     try {
-        //console.log(req.url);
         if(req.url === "/login" || req.url === "/register" || req.url === "/forgot-password" || req.url === "/confirm-otp"){
             return next();
         }
