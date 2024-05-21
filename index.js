afb0d0aad38a13119596f41816efeca8d16a2527
HaiquanZ <nn764562@gmail.com>
2024-05-21 23:40:58 +0700

    update

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
 const authorize = require('./src/middlewares/auth');
+const bodyParser = require("body-parser");
 
 const app = express();
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
+app.use(bodyParser. text({type: '/'}));
 
 app.use('/user', authorize, proxy('http://localhost:8001'))
 app.use('/group', authorize, proxy('http://localhost:8002'))
